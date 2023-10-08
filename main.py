c2b825a3f37b04fb870ef3185a88e2a9f84d4d6c
Родион Падаков <rpadakov@yandex.ru>
2023-10-08 12:22:17 +0800

    Add docstrings

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from random import randint
+
 from graphic_arts.start_game_banner import run_screensaver
 
-def attack (char_name: str, char_class: str) -> str:
+
+def attack(char_name: str, char_class: str) -> str:
+    """Form a message about attack action based on the character class.
+    and return this message
+
+    Keyword arguments:
+    char_name -- the name of the character specified by the user
+    char_class -- the class of the character specified by the user
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,7 +23,14 @@ def attack (char_name: str, char_class: str) -> str:
     return (f'{char_name} не атаковал')
 
 
-def defence (char_name: str, char_class: str) -> str:
+def defence(char_name: str, char_class: str) -> str:
+    """Form a message about deffence action based on the character class.
+    and return this message
+
+    Keyword arguments:
+    char_name -- the name of the character specified by the user
+    char_class -- the class of the character specified by the user
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +41,13 @@ def defence (char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Form a message about special action based on the character class.
+    and return this message
+
+    Keyword arguments:
+    char_name -- the name of the character specified by the user
+    char_class -- the class of the character specified by the user
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -38,6 +61,13 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Form a message about character class specified by the user.
+    Generate a suggestion for the user to choose an action
+
+    Keyword arguments:
+    char_name -- the name of the character specified by the user
+    char_class -- the class of the character specified by the user
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +92,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Generate a suggestion for the user to choose character class
+    return a message associated with each class
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -93,6 +126,4 @@ if __name__ == '__main__':
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class)) 
-
-
+    print(start_training(char_name, char_class))
